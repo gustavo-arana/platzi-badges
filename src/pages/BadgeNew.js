42d84c2e7058de9507734662507aae5f7fbcff47
Gustavo Arana <gustavo.arana91@gmail.com>
2020-05-31 21:56:39 -0500

    State Component

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import NavBar from "../components/NavBar";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName="Gustavo"
+                lastName="Arana"
+                avatarUrl="https://s.gravatar.com/avatar/b982d96529461949899e5b9e6e93272f?s=80"
+                jobTitle="Developer Enginner"
+                twitter="gabo_678"
+              />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
